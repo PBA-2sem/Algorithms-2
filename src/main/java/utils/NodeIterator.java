e3e539413312d4777599f6873fd8b1a19d014f7c
Stanislav Novitski <Stanislav.Novitski@ibm.com>
2020-03-03 21:51:04 +0100

    added heap sort code snippet into class

---
+++ src/main/java/utils/NodeIterator.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.util.Iterator;
+
+public class NodeIterator<T> implements Iterator<T> {
+
+    private Node<T> step;
+
+    public NodeIterator(Node<T> first) {
+        this.step = first;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return step != null;
+    }
+
+    @Override
+    public T next() {
+        T value = step.getValue();
+        step = step.getNext();
+        return value;
+    }
+
+}
